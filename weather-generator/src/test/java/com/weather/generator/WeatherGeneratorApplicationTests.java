96e33971456086bc1c0dc40d7daf54c83669d5e5
moham <mohammad-alsharif10@outlook.com>
2023-03-17 14:43:21 +0200

    init

---
+++ weather-generator/src/test/java/com/weather/generator/WeatherGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weather.generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
